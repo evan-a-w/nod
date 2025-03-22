3f1c53d4c812adab90cfd306449667995abcf630
Evan Williams <evanawilliams@live.com>
2025-03-23 00:35:08 +0800

    😊

--- lib/eir.ml
+++ lib/eir.ml
@@ -2,11 +2,10 @@ open! Core
 include Ir
 include Initial_transform.Make_with_block (Ir)
 
-module Tag = struct
-  type t =
-    | None
-    | Constant of int
-  [@@deriving sexp]
+module Tags = struct
+  type t = { constant : int option } [@@deriving sexp]
+
+  let empty = { constant = None }
 end
 
 module Loc = struct
@@ -37,7 +36,7 @@ end
 module Var = struct
   type t =
     { id : string
-    ; mutable type_ : Tag.t
+    ; mutable tags : Tags.t
     ; mutable loc : Loc.t
     ; uses : Loc.Hash_set.t
     }
@@ -72,7 +71,7 @@ module Opt = struct
     let t = create ssa in
     let define ~loc id =
       let var =
-        { Var.id; type_ = Tag.None; loc; uses = Loc.Hash_set.create () }
+        { Var.id; tags = Tags.empty; loc; uses = Loc.Hash_set.create () }
       in
       Hashtbl.set t.vars ~key:id ~data:var;
       t.vars_set <- Set.add t.vars_set id
@@ -109,6 +108,24 @@ module Opt = struct
     block.Block.instructions
     <- Vec.filter block.Block.instructions ~f:(Fn.non (phys_equal instr))
 
+  and remove_arg_from_parent t ~parent ~idx =
+    parent.Block.terminal
+    <- Ir.map_args
+         parent.Block.terminal
+         ~f:
+           (List.filteri ~f:(fun i id ->
+              match i = idx, Hashtbl.find t.vars id with
+              | false, _ -> true
+              | true, None -> false
+              | true, Some var ->
+                (* kill the tang *)
+                Hash_set.filter_inplace var.uses ~f:(fun loc ->
+                  match loc.Loc.where with
+                  | Loc.Block_arg _ -> true
+                  | Instr i -> not (phys_equal i parent.terminal));
+                try_kill_var t ~id;
+                false))
+
   and remove_arg t ~block ~arg =
     let idx =
       Vec.findi block.Block.args ~f:(fun i s ->
@@ -117,23 +134,7 @@ module Opt = struct
     in
     block.args <- Vec.filter block.args ~f:(Fn.non (String.equal arg));
     Vec.iter block.parents ~f:(fun parent ->
-      parent.terminal
-      <- Ir.map_args
-           parent.terminal
-           ~f:
-             (List.filteri ~f:(fun i id ->
-                match i = idx, Hashtbl.find t.vars id with
-                | false, _ -> true
-                | true, None -> false
-                | true, Some var ->
-                  (* kill the tang *)
-                  Hash_set.filter_inplace var.uses ~f:(fun loc ->
-                    match loc.Loc.where with
-                    | Loc.Block_arg _ -> true
-                    | Instr i -> not (phys_equal i parent.terminal));
-                  try_kill_var t ~id;
-                  false)));
-    ()
+      remove_arg_from_parent t ~parent ~idx)
 
   and kill_definition t ~id =
     match Hashtbl.find t.vars id with
@@ -165,9 +166,8 @@ module Opt = struct
       let loc =
         Hash_set.min_elt ~compare:Loc.compare var.uses |> Option.value_exn
       in
-      if
-        phys_equal loc.block var.loc.block
-        && Loc.is_terminal_for_block loc ~block:var.loc.block
+      if phys_equal loc.block var.loc.block
+         && Loc.is_terminal_for_block loc ~block:var.loc.block
       then kill_definition t ~id:var.id
       else ()
     | _ ->
