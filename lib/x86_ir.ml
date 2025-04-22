bb5643645cd9bf58daaf41df758ad43100ef171a
Evan Williams <evanawilliams@live.com>
2025-04-23 00:10:53 +0800

    😊

--- lib/x86_ir.ml
+++ lib/x86_ir.ml
@@ -2,25 +2,31 @@ open Core
 open Ir
 
 module Reg = struct
-  type t =
-    | Unallocated of Var.t
-    | RBP
-    | RSP
-    | RAX
-    | RBX
-    | RCX
-    | RDX
-    | RSI
-    | RDI
-    | R8
-    | R9
-    | R10
-    | R11
-    | R12
-    | R13
-    | R14
-    | R15
-  [@@deriving sexp, equal, compare, hash]
+  module T = struct
+    type t =
+      | Unallocated of Var.t
+      | RBP
+      | RSP
+      | RAX
+      | RBX
+      | RCX
+      | RDX
+      | RSI
+      | RDI
+      | R8
+      | R9
+      | R10
+      | R11
+      | R12
+      | R13
+      | R14
+      | R15
+    [@@deriving sexp, equal, compare, hash]
+  end
+
+  include T
+  include Comparable.Make (T)
+  include Hashable.Make (T)
 end
 
 type operand =
@@ -42,3 +48,68 @@ type t =
   | JNE of string
   | RET of operand
 [@@deriving sexp, equal, compare, hash]
+
+let vars_of_reg = function
+  | Reg.Unallocated v -> Var.Set.singleton v
+  | _ -> Var.Set.empty
+;;
+
+let vars_of_operand = function
+  | Reg r -> vars_of_reg r
+  | Imm _ -> Var.Set.empty
+  | Mem (r, _disp) -> vars_of_reg r
+;;
+
+let map_reg r ~f =
+  match r with
+  | Reg.Unallocated v -> Reg.Unallocated (f v)
+  | _ -> r
+;;
+
+let map_operand op ~f =
+  match op with
+  | Reg r -> Reg (map_reg r ~f)
+  | Imm _ -> op
+  | Mem (r, disp) -> Mem (map_reg r ~f, disp)
+;;
+
+let defs (ins : t) : Var.Set.t =
+  match ins with
+  | MOV (dst, _) -> vars_of_operand dst
+  | ADD (dst, _) | SUB (dst, _) | MUL (dst, _) -> vars_of_operand dst
+  | IDIV _ -> Var.Set.empty (* RAX/RDX: real regs *)
+  | RET _ | CMP _ | LABEL _ | JMP _ | JE _ | JNE _ -> Var.Set.empty
+;;
+
+let uses (ins : t) : Var.Set.t =
+  match ins with
+  | MOV (_, src) -> vars_of_operand src
+  | ADD (dst, src) | SUB (dst, src) | MUL (dst, src) ->
+    Set.union (vars_of_operand dst) (vars_of_operand src)
+  | IDIV op -> Set.union (vars_of_operand op) (vars_of_operand (Reg Reg.RAX))
+  | CMP (a, b) -> Set.union (vars_of_operand a) (vars_of_operand b)
+  | RET op -> vars_of_operand op
+  | LABEL _ | JMP _ | JE _ | JNE _ -> Var.Set.empty
+;;
+
+let map_defs (ins : t) ~(f : Var.t -> Var.t) : t =
+  match ins with
+  | MOV (dst, src) -> MOV (map_operand dst ~f, src)
+  | ADD (dst, src) -> ADD (map_operand dst ~f, src)
+  | SUB (dst, src) -> SUB (map_operand dst ~f, src)
+  | MUL (dst, src) -> MUL (map_operand dst ~f, src)
+  | IDIV _ | LABEL _ | JMP _ | CMP (_, _) | JE _ | JNE _ | RET _ ->
+    ins (* no virtual‑defs *)
+;;
+
+let map_uses (ins : t) ~(f : Var.t -> Var.t) : t =
+  match ins with
+  | MOV (dst, src) -> MOV (dst, map_operand src ~f)
+  | ADD (dst, src) -> ADD (map_operand dst ~f, map_operand src ~f)
+  | SUB (dst, src) -> SUB (map_operand dst ~f, map_operand src ~f)
+  | MUL (dst, src) -> MUL (map_operand dst ~f, map_operand src ~f)
+  | IDIV op -> IDIV (map_operand op ~f)
+  | CMP (a, b) -> CMP (map_operand a ~f, map_operand b ~f)
+  | RET op -> RET (map_operand op ~f)
+  | LABEL _ | JMP _ | JE _ | JNE _ -> ins (* no virtual‑uses *)
+;;
--- lib/x86_regalloc.ml
+++ lib/x86_regalloc.ml
@@ -22,14 +22,108 @@ module Interval = struct
   include Hashable.Make (T)
 end
 
-module State = struct
+module Stack_slot = Int
+
+module Stack_slots = struct
   type t =
-    { free_regs : Reg.t list
-    ; mappings : Reg.t Var.Map.t
-    ; live_intervals : Var.t Interval.Map.t
-    ; intervals_by_end_point : Interval.t Int.Map.t
+    { mutable free_slots : Stack_slot.Set.t
+    ; mutable alloc_from : Stack_slot.t
     }
   [@@deriving sexp]
+
+  let alloc t =
+    match Set.min_elt t.free_slots with
+    | Some x ->
+      t.free_slots <- Set.remove t.free_slots x;
+      x
+    | None ->
+      t.alloc_from <- t.alloc_from + 1;
+      t.alloc_from - 1
+  ;;
+
+  let free t ~slot = t.free_slots <- Set.add t.free_slots slot
+  let create ~alloc_from = { free_slots = Stack_slot.Set.empty; alloc_from }
 end
 
-let process _instrs = ()
+module Mapping = struct
+  type t =
+    | Reg of Reg.t
+    | Stack_slot of Stack_slot.t
+  [@@deriving sexp, compare, equal, hash]
+end
+
+module State = struct
+  type t =
+    { mutable free_regs : Reg.t list
+    ; mappings : Mapping.t Var.Table.t
+    ; mutable live_intervals : Var.t Interval.Map.t
+    ; stack_slots : Stack_slots.t
+    }
+  [@@deriving sexp]
+
+  let get_reg t =
+    match t.free_regs with
+    | [] -> None
+    | reg :: rest ->
+      t.free_regs <- rest;
+      Some reg
+  ;;
+
+  let rec to_spill ~don't_spill ?current t =
+    let ((interval, _) as res) =
+      match current with
+      | None -> Map.min_elt_exn t.live_intervals
+      | Some x ->
+        Map.closest_key t.live_intervals `Greater_than x |> Option.value_exn
+    in
+    if not (Set.mem don't_spill interval.reg)
+    then (
+      t.live_intervals <- Map.remove t.live_intervals interval;
+      res)
+    else to_spill ~don't_spill ~current:interval t
+  ;;
+
+  let get_reg_or_spill ~don't_spill t =
+    match get_reg t with
+    | Some x -> `Reg x
+    | None ->
+      let interval, var_to_spill = to_spill ~don't_spill t in
+      let stack_slot = Stack_slots.alloc t.stack_slots in
+      Hashtbl.set t.mappings ~key:var_to_spill ~data:(Stack_slot stack_slot);
+      `Spill (MOV (Mem (RSP, -stack_slot), Reg interval.reg), interval.reg)
+  ;;
+
+  let create ~alloc_from =
+    { free_regs
+    ; mappings = Var.Table.create ()
+    ; live_intervals = Interval.Map.empty
+    ; stack_slots = Stack_slots.create ~alloc_from
+    }
+  ;;
+end
+
+(* let process ~stack_offset instrs = *)
+(*   let state = State.create ~alloc_from:stack_offset in *)
+(*   Vec.concat_map instrs ~f:(fun instr -> *)
+
+(*       let uses' = uses instr in *)
+(*       let defs' = defs instr in *)
+(*       let both = (Set.union uses' defs') in *)
+(*       let don't_spill = Set.filter_map (module Reg) both ~f:(fun this -> *)
+(*           match Hashtbl.find state.mappings this with *)
+(*           | Some (Reg r) -> Some r *)
+(*           | Some (Stack_slot _) | None -> None *)
+(*         ) in *)
+(*       let allocs_this_time = Set.fold ~init:Var.Set.empty both ~f:(fun acc this -> *)
+(*           match Hashtbl.find state.mappings this with *)
+(*           | Some (Reg r) ->  *)
+
+(*         ) *)
+
+(*       in *)
+(*        let new_mappings = uses instr |> Set.to_list |> List.map ~f:(fun use -> *)
+
+(*         ) *)
+
+(*     ) *)
+(* ;; *)
