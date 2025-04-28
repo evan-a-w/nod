62626a3740e149cb57da23ae6e465ced2c843b1c
Evan Williams <evanawilliams@live.com>
2025-04-28 23:05:58 +0800

    😊

--- lib/test_x86.ml
+++ lib/test_x86.ml
@@ -8,8 +8,18 @@ let test s =
     print_s [%sexp (x86 : (string, string) X86_ir.instr Vec.t)]
 ;;
 
-let%expect_test "e2" = test Examples.Textual.e2;
-  [%expect {|
+let%expect_test "e2" =
+  test Examples.Textual.e2;
+  [%expect
+    {|
     ((LABEL start) (JMP ifFalse) (LABEL ifFalse) (MOV (Reg RAX) (Imm 20))
      (PAR_MOV (((Reg RAX) (Reg RAX)))) (JMP end) (LABEL end)
      (MOV (Reg RAX) (Reg RAX)) (RET (Reg RAX))) |}]
+;;
+
+let%expect_test "c2" =
+  test Examples.Textual.c2;
+  [%expect
+    {|
+    ((LABEL entry) (MOV (Reg RAX) (Imm 5)) (RET (Reg RAX))) |}]
+;;
