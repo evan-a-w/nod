f3c39b09831e174e4b1f4d4191f26aa80d204fc8
Evan Williams <evanawilliams@live.com>
2025-10-19 22:58:29 +0800

    😊

--- test_lib/test_x86_asm.ml
+++ test_lib/test_x86_asm.ml
@@ -17,10 +17,11 @@ let%expect_test "super triv lowers to assembly" =
     .text
     .globl root
     root:
-      mov rbp, rsp
       push rbp
       push r14
       push r15
+      mov rbp, rsp
+      add rbp, 24
       jmp root__a
     root__a:
       mov r15, 10
@@ -46,11 +47,12 @@ let%expect_test "branches lower with labels" =
     .text
     .globl root
     root:
-      mov rbp, rsp
       push rbp
       push r13
       push r14
       push r15
+      mov rbp, rsp
+      add rbp, 32
       jmp root__a
     root__a:
       mov r15, 10
@@ -91,5 +93,71 @@ let%expect_test "branches lower with labels" =
     root__intermediate_c_to_end:
       mov r15, r14
       jmp root__end
+    |}]
+;;
+
+let%expect_test "recursive fib" =
+  compile_and_lower Examples.Textual.fib_recursive;
+  [%expect
+    {|
+    .intel_syntax noprefix
+    .text
+    .globl fib
+    fib:
+      push rbp
+      push r13
+      push r14
+      push r15
+      mov rbp, rsp
+      add rbp, 32
+      mov r14, rdi
+      jmp fib___root
+    fib___root:
+      cmp r14, 0
+      jne fib__intermediate__root_to_check1_
+      jmp fib__intermediate__root_to_ret_1
+    fib__intermediate__root_to_check1_:
+      jmp fib__check1_
+    fib__check1_:
+      mov r15, r14
+      sub r15, 1
+      cmp r15, 0
+      jne fib__intermediate_check1__to_rec
+      jmp fib__intermediate_check1__to_ret_1
+    fib__intermediate_check1__to_rec:
+      jmp fib__rec
+    fib__rec:
+      push rax
+      mov rdi, r15
+      call fib
+      mov r14, rax
+      pop rax
+      mov r13, r15
+      sub r13, 1
+      push rax
+      mov rdi, r13
+      call fib
+      mov r13, rax
+      pop rax
+      add r14, r13
+      mov rax, r14
+      jmp fib__fib__epilogue
+    fib__fib__epilogue:
+      mov rsp, rbp
+      sub rsp, 32
+      pop r15
+      pop r14
+      pop r13
+      pop rbp
+      ret
+    fib__intermediate_check1__to_ret_1:
+      jmp fib__ret_1
+    fib__ret_1:
+      mov r14, 1
+      mov rax, r14
+      jmp fib__fib__epilogue
+    fib__intermediate__root_to_ret_1:
+      mov r15, r13
+      jmp fib__ret_1
     |}]
 ;;
