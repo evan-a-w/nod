0255a51d0f9a001feadad2564d0694d9f212d44b
Evan Williams <evanawilliams@live.com>
2025-12-06 23:17:43 +0800

    😊

--- x86_backend/x86_backend.ml
+++ x86_backend/x86_backend.ml
@@ -78,4 +78,51 @@ module For_testing = struct
           ])
       |> Table.print_records)
   ;;
+
+  let compute_assignments (fn : Function.t) =
+    let var_classes = Regalloc.collect_var_classes fn.root in
+    let class_of_var var =
+      Hashtbl.find var_classes var |> Option.value ~default:X86_reg.Class.I64
+    in
+    let reg_numbering = Reg_numbering.create fn.root in
+    let (module Calc_liveness) =
+      Calc_liveness.var ~treat_block_args_as_defs:false ~reg_numbering
+    in
+    let liveness_state = Calc_liveness.Liveness_state.create ~root:fn.root in
+    let interference_graph =
+      Interference_graph.create
+        (module Calc_liveness)
+        ~liveness_state
+        ~root:fn.root
+    in
+    let ~assignments, ~don't_spill = Regalloc.initialize_assignments fn.root in
+    List.iter [ X86_reg.Class.I64; X86_reg.Class.F64 ] ~f:(fun class_ ->
+      Regalloc.run_sat
+        ~dump_crap:false
+        ~reg_numbering
+        ~interference_graph
+        ~assignments
+        ~don't_spill
+        ~class_of_var
+        ~class_);
+    assignments
+  ;;
+
+  let print_assignments (functions : Function.t String.Map.t) =
+    Map.iteri functions ~f:(fun ~key:function_name ~data:fn ->
+      let fn = Instruction_selection.run fn in
+      let assignments_table = compute_assignments fn in
+      let assignments =
+        assignments_table
+        |> Hashtbl.to_alist
+        |> List.sort
+             ~compare:
+               (Comparable.lift String.compare ~f:(fun (var, _) -> Var.name var))
+      in
+      print_s
+        [%message
+          ""
+            ~function_name:(function_name : string)
+            (assignments : (Var.t * Assignment.t) list)])
+  ;;
 end
