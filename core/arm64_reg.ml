d42c4b032a9e72de2bff7c1c208c19b5b826345d
Evan Williams <evanawilliams@live.com>
2025-12-21 14:21:45 +0800

    😊

--- core/arm64_reg.ml
+++ core/arm64_reg.ml
@@ -488,24 +488,69 @@ let all_physical =
 
 let allocable ~class_ =
   match class_ with
-  | Class.I64 -> [ rax; rbx; rcx; rdx; r8; r9; r10; r11; r12; r13; r14; r15 ]
+  | Class.I64 ->
+    [ x0
+    ; x1
+    ; x2
+    ; x3
+    ; x4
+    ; x5
+    ; x6
+    ; x7
+    ; x8
+    ; x9
+    ; x10
+    ; x11
+    ; x12
+    ; x13
+    ; x14
+    ; x15
+    ; x16
+    ; x17
+    ; x19
+    ; x20
+    ; x21
+    ; x22
+    ; x23
+    ; x24
+    ; x25
+    ; x26
+    ; x27
+    ; x28
+    ]
   | Class.F64 ->
-    [ xmm0
-    ; xmm1
-    ; xmm2
-    ; xmm3
-    ; xmm4
-    ; xmm5
-    ; xmm6
-    ; xmm7
-    ; xmm8
-    ; xmm9
-    ; xmm10
-    ; xmm11
-    ; xmm12
-    ; xmm13
-    ; xmm14
-    ; xmm15
+    [ v0
+    ; v1
+    ; v2
+    ; v3
+    ; v4
+    ; v5
+    ; v6
+    ; v7
+    ; v8
+    ; v9
+    ; v10
+    ; v11
+    ; v12
+    ; v13
+    ; v14
+    ; v15
+    ; v16
+    ; v17
+    ; v18
+    ; v19
+    ; v20
+    ; v21
+    ; v22
+    ; v23
+    ; v24
+    ; v25
+    ; v26
+    ; v27
+    ; v28
+    ; v29
+    ; v30
+    ; v31
     ]
 ;;
 
