ffcce234a04ee288a2c6976b8dcfc4ded0d9aae3
Evan Williams <evanawilliams@live.com>
2025-12-21 23:45:05 +0800

    😊

--- test_lib/import.ml
+++ test_lib/import.ml
@@ -4,16 +4,9 @@ include Nod
 
 let host_system = Lazy.force Nod.host_system
 
-let should_run_arm64_tests =
-  match Core.Sys.getenv "NOD_RUN_ARM64_TESTS" with
-  | Some "1" | Some "true" | Some "TRUE" -> true
-  | _ -> false
-;;
-
 let test_architectures : [ `X86_64 | `Arm64 | `Other ] list =
   match Lazy.force Nod.host_arch with
-  | `Arm64 when should_run_arm64_tests -> [ `X86_64; `Arm64 ]
-  | `Arm64 -> [ `X86_64 ]
+  | `Arm64 -> [ `X86_64; `Arm64 ]
   | `X86_64 -> [ `X86_64 ]
   | `Other -> []
 ;;
@@ -24,11 +17,6 @@ let arch_to_string = function
   | `Other -> "other"
 ;;
 
-let compile_and_execute_on_arch
-  arch
-  ?harness
-  ?opt_flags
-  program
-  =
+let compile_and_execute_on_arch arch ?harness ?opt_flags program =
   Nod.compile_and_execute ~arch ~system:host_system ?harness ?opt_flags program
 ;;
