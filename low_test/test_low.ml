1ac66751fa80f6f4730eb6b4b56891f9aceb8a47
Evan Williams <evanawilliams@live.com>
2026-01-01 16:09:30 +0800

    😊

--- low_test/test_low.ml
+++ low_test/test_low.ml
@@ -2,9 +2,9 @@ open! Core
 open! Nod_core
 
 let test_ok s =
-  match Nod_clike.Clike.lower_string s with
+  match Nod_low.Low.lower_string s with
   | Ok _ -> print_endline "ok"
-  | Error err -> Nod_clike.Error.to_string err |> print_endline
+  | Error err -> Nod_low.Error.to_string err |> print_endline
 ;;
 
 let%expect_test "struct alloca and field access" =
@@ -19,6 +19,28 @@ i64 sum(i64 x, i64 y) {
   p->a = x;
   p->b = y;
   return p->a + p->b;
+}
+|}
+  |> test_ok;
+  [%expect {| ok |}]
+;;
+
+let%expect_test "struct value locals and by-value calls" =
+  {|
+struct Pair {
+  a: i64;
+  b: i64;
+};
+
+i64 sum(struct Pair p) {
+  return p.a + p.b;
+}
+
+i64 main(i64 x) {
+  struct Pair q;
+  q.a = x;
+  q.b = x;
+  return sum(q);
 }
 |}
   |> test_ok;
