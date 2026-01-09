f08a3236b65e124a003f2d24c453d9dd869cf732
Evan Williams <evanawilliams@live.com>
2026-01-10 01:33:24 +0800

    😊

--- test_lib/test_ppx_nod.ml
+++ test_lib/test_ppx_nod.ml
@@ -8,6 +8,7 @@ module Dsl = struct
   let var v = L.Var v
   let mov src ~dest = Ir.move dest src
   let add src1 src2 ~dest = Ir.add { dest; src1; src2 }
+  let call1 fn arg ~dest = Ir.call ~fn ~results:[ dest ] ~args:[ arg ]
 end
 
 let%expect_test "block builder" =
@@ -68,5 +69,77 @@ let%expect_test "loop label" =
     {|
     ((%entry (args ())
       (instrs ((Branch (Uncond ((block ((id_hum loop) (args ()))) (args ()))))))))
+    |}]
+;;
+
+let%expect_test "compose nod functions" =
+  let mk_double =
+    [%nod fun (x : i64) ->
+      let (twice : i64) = Dsl.add (Dsl.var x) (Dsl.var x) in
+      return (Dsl.var twice)
+    ]
+  in
+  let mk_offset delta =
+    [%nod fun (x : i64) ->
+      let (twice : i64) = Dsl.call1 "double" (Dsl.var x) in
+      let (shifted : i64) = Dsl.add (Dsl.var twice) (Dsl.lit delta) in
+      return (Dsl.var shifted)
+    ]
+  in
+  let mk_entry start =
+    [%nod fun (input : i64) ->
+      let (seed : i64) = Dsl.add (Dsl.var input) (Dsl.lit start) in
+      let (out : i64) = Dsl.call1 "offset" (Dsl.var seed) in
+      return (Dsl.var out)
+    ]
+  in
+  let funcs =
+    [ mk_double ~name:"double"
+    ; mk_offset 5L ~name:"offset"
+    ; mk_entry 3L ~name:"entry"
+    ]
+  in
+  List.iter funcs ~f:(fun fn ->
+    Block.iter_and_update_bookkeeping (Function.root fn) ~f:(fun _ -> ());
+    Function.print_verbose fn);
+  [%expect
+    {|
+    ((call_conv Default)
+     (root
+      ((%entry (args (((name x) (type_ I64))))
+        (instrs
+         ((Add
+           ((dest ((name twice) (type_ I64))) (src1 (Var ((name x) (type_ I64))))
+            (src2 (Var ((name x) (type_ I64))))))
+          (Return (Var ((name twice) (type_ I64)))))))))
+     (args (((name x) (type_ I64)))) (name double) (prologue ()) (epilogue ())
+     (bytes_for_clobber_saves 0) (bytes_for_padding 0) (bytes_for_spills 0)
+     (bytes_statically_alloca'd 0))
+    ((call_conv Default)
+     (root
+      ((%entry (args (((name x) (type_ I64))))
+        (instrs
+         ((Call (fn double) (results (((name twice) (type_ I64))))
+           (args ((Var ((name x) (type_ I64))))))
+          (Add
+           ((dest ((name shifted) (type_ I64)))
+            (src1 (Var ((name twice) (type_ I64)))) (src2 (Lit 5))))
+          (Return (Var ((name shifted) (type_ I64)))))))))
+     (args (((name x) (type_ I64)))) (name offset) (prologue ()) (epilogue ())
+     (bytes_for_clobber_saves 0) (bytes_for_padding 0) (bytes_for_spills 0)
+     (bytes_statically_alloca'd 0))
+    ((call_conv Default)
+     (root
+      ((%entry (args (((name input) (type_ I64))))
+        (instrs
+         ((Add
+           ((dest ((name seed) (type_ I64)))
+            (src1 (Var ((name input) (type_ I64)))) (src2 (Lit 3))))
+          (Call (fn offset) (results (((name out) (type_ I64))))
+           (args ((Var ((name seed) (type_ I64))))))
+          (Return (Var ((name out) (type_ I64)))))))))
+     (args (((name input) (type_ I64)))) (name entry) (prologue ()) (epilogue ())
+     (bytes_for_clobber_saves 0) (bytes_for_padding 0) (bytes_for_spills 0)
+     (bytes_statically_alloca'd 0))
     |}]
 ;;
