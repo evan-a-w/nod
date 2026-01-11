42a262817be3f3a33d18194095037bb58a3c44b9
Evan Williams <evanawilliams@live.com>
2026-01-11 13:16:53 +0800

    😊

---
+++ test_lib/test_nod_ppx.ml
@@ -0,0 +1,66 @@
+open! Core
+open! Import
+
+let block_of_instrs instrs =
+  instrs
+  |> Dsl.Instr.process
+  |> Result.ok_exn
+  |> Cfg.process
+  |> fun { root; _ } -> root
+;;
+
+let%expect_test "nod block from let%named" =
+  let root =
+    [%nod
+      let%named tmp = mov (lit 1L) in
+      return tmp]
+  in
+  print_s (Block.to_sexp_verbose root);
+  [%expect
+    {| ((%entry (args ())
+  (instrs
+   ((Move ((name tmp) (type_ I64)) (Lit 1))
+    (Return (Var ((name tmp) (type_ I64)))))))) |}]
+;;
+
+let%expect_test "nod seq embeds instruction list" =
+  let root =
+    [%nod
+      seq [ Instr.ir Ir0.Noop; Instr.ir Ir0.Noop ];
+      return (lit 0L)]
+  in
+  let instrs = Vec.to_list root.instructions in
+  let noop_count =
+    List.count instrs ~f:(function
+      | Ir0.Noop -> true
+      | _ -> false)
+  in
+  print_s [%sexp (Vec.length root.instructions : int)];
+  print_s [%sexp (noop_count : int)];
+  [%expect {|
+    2
+    2
+  |}]
+;;
+
+let%expect_test "nod fun builds args and return type" =
+  let fn =
+    [%nod fun (a : int64) (b : int64) ->
+      let%named sum = add a b in
+      return sum]
+  in
+  let unnamed = Dsl.Fn.unnamed fn in
+  print_s [%sexp (Dsl.Fn.Unnamed.args unnamed : Var.t list)];
+  print_s [%sexp (Dsl.Fn.Unnamed.ret unnamed : Type.t)];
+  let root = block_of_instrs (Dsl.Fn.Unnamed.instrs unnamed) in
+  print_s (Block.to_sexp_verbose root);
+  [%expect
+    {| (((name a) (type_ I64)) ((name b) (type_ I64)))
+I64
+((%entry (args ())
+  (instrs
+   ((Add
+     ((dest ((name sum) (type_ I64))) (src1 (Var ((name a) (type_ I64))))
+      (src2 (Var ((name b) (type_ I64))))))
+    (Return (Var ((name sum) (type_ I64)))))))) |}]
+;;
