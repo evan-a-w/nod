b34b508d29c83fb74da1b827d33b2249c17d30de
Evan Williams <evanawilliams@live.com>
2026-01-11 17:07:09 +0800

    😊

--- dsl/dsl.mli
+++ dsl/dsl.mli
@@ -23,41 +23,44 @@ module Atom : sig
 end
 
 module Instr : sig
-  type t
+  type 'ret t
 
-  val ir : string Ir0.t -> t
+  val ir : string Ir0.t -> 'ret t
 
   val process
     :  ?root_name:string
-    -> t list
+    -> 'ret t list
     -> (Eir.raw_block, Nod_error.t) Result.t
 end
 
 module Fn : sig
-  type 'a t
+  type ('fn, 'ret) t
 
-  val name : 'a t -> string
-  val function_ : 'a t -> (Eir.raw_block Function0.t', Nod_error.t) Result.t
+  val name : ('fn, 'ret) t -> string
+
+  val function_
+    :  ('fn, 'ret) t
+    -> (Eir.raw_block Function0.t', Nod_error.t) Result.t
 
   module Unnamed : sig
-    type 'a t
+    type ('fn, 'ret) t
 
-    val const : 'a Type_repr.t -> Instr.t list -> 'a t
-    val const_with_return : 'a Atom.t -> Instr.t list -> 'a t
-    val with_arg : 'ret t -> Var.t -> ('a -> 'ret) t
-    val args : 'a t -> Var.t list
-    val ret : 'a t -> Type.t
-    val instrs : 'a t -> Instr.t list
+    val const : 'ret Type_repr.t -> 'ret Instr.t list -> ('ret, 'ret) t
+    val const_with_return : 'ret Atom.t -> 'ret Instr.t list -> ('ret, 'ret) t
+    val with_arg : ('fn, 'ret) t -> Var.t -> ('a -> 'fn, 'ret) t
+    val args : ('fn, 'ret) t -> Var.t list
+    val ret : ('fn, 'ret) t -> Type.t
+    val instrs : ('fn, 'ret) t -> 'ret Instr.t list
   end
 
-  val unnamed : 'a t -> 'a Unnamed.t
-  val create : unnamed:'a Unnamed.t -> name:string -> 'a t
+  val unnamed : ('fn, 'ret) t -> ('fn, 'ret) Unnamed.t
+  val create : unnamed:('fn, 'ret) Unnamed.t -> name:string -> ('fn, 'ret) t
 
   module Packed : sig
     type t
   end
 
-  val pack : _ t -> Packed.t
+  val pack : (_, _) t -> Packed.t
 end
 
 (** meta functions *)
@@ -66,62 +69,101 @@ val program : functions:Fn.Packed.t list -> globals:Global.t list -> Eir.input
 
 (** builder functions *)
 
-val return : 'a Atom.t -> Instr.t
+val return : 'a Atom.t -> 'a Instr.t
 val lit : Int64.t -> int64 Atom.t
 val var : Var.t -> 'a Atom.t
 val global : Global.t -> ptr Atom.t
-val mov : string -> 'a Atom.t -> 'a Atom.t * Instr.t
-val add : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val sub : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val mul : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val div : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val mod_ : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val and_ : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val or_ : string -> int64 Atom.t -> int64 Atom.t -> int64 Atom.t * Instr.t
-val ptr_add : string -> ptr Atom.t -> int64 Atom.t -> ptr Atom.t * Instr.t
-val ptr_sub : string -> ptr Atom.t -> int64 Atom.t -> ptr Atom.t * Instr.t
-val ptr_diff : string -> ptr Atom.t -> ptr Atom.t -> int64 Atom.t * Instr.t
+val mov : string -> 'a Atom.t -> 'a Atom.t * 'ret Instr.t
+val add
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val sub
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val mul
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val div
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val mod_
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val and_
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val or_
+  :  string
+  -> int64 Atom.t
+  -> int64 Atom.t
+  -> int64 Atom.t * 'ret Instr.t
+
+val ptr_add : string -> ptr Atom.t -> int64 Atom.t -> ptr Atom.t * 'ret Instr.t
+val ptr_sub : string -> ptr Atom.t -> int64 Atom.t -> ptr Atom.t * 'ret Instr.t
+val ptr_diff : string -> ptr Atom.t -> ptr Atom.t -> int64 Atom.t * 'ret Instr.t
 
 val fadd
   :  string
   -> float64 Atom.t
   -> float64 Atom.t
-  -> float64 Atom.t * Instr.t
+  -> float64 Atom.t * 'ret Instr.t
 
 val fsub
   :  string
   -> float64 Atom.t
   -> float64 Atom.t
-  -> float64 Atom.t * Instr.t
+  -> float64 Atom.t * 'ret Instr.t
 
 val fmul
   :  string
   -> float64 Atom.t
   -> float64 Atom.t
-  -> float64 Atom.t * Instr.t
+  -> float64 Atom.t * 'ret Instr.t
 
 val fdiv
   :  string
   -> float64 Atom.t
   -> float64 Atom.t
-  -> float64 Atom.t * Instr.t
+  -> float64 Atom.t * 'ret Instr.t
 
-val load : string -> ptr Atom.t -> int64 Atom.t * Instr.t
-val load_ptr : string -> ptr Atom.t -> ptr Atom.t * Instr.t
-val load_f64 : string -> ptr Atom.t -> float64 Atom.t * Instr.t
-val store : 'a Atom.t -> ptr Atom.t -> Instr.t
-val load_addr : string -> ptr Atom.t -> int -> int64 Atom.t * Instr.t
-val load_addr_ptr : string -> ptr Atom.t -> int -> ptr Atom.t * Instr.t
-val load_addr_f64 : string -> ptr Atom.t -> int -> float64 Atom.t * Instr.t
-val store_addr : 'a Atom.t -> ptr Atom.t -> int -> Instr.t
-val alloca : string -> int64 Atom.t -> ptr Atom.t * Instr.t
-val cast : string -> Type.t -> 'a Atom.t -> 'b Atom.t * Instr.t
-val call0 : string -> 'ret Fn.t -> 'ret Atom.t * Instr.t
-val call1 : string -> ('a -> 'ret) Fn.t -> 'a Atom.t -> 'ret Atom.t * Instr.t
+val load : string -> ptr Atom.t -> int64 Atom.t * 'ret Instr.t
+val load_ptr : string -> ptr Atom.t -> ptr Atom.t * 'ret Instr.t
+val load_f64 : string -> ptr Atom.t -> float64 Atom.t * 'ret Instr.t
+val store : 'a Atom.t -> ptr Atom.t -> 'ret Instr.t
+val load_addr : string -> ptr Atom.t -> int -> int64 Atom.t * 'ret Instr.t
+val load_addr_ptr : string -> ptr Atom.t -> int -> ptr Atom.t * 'ret Instr.t
+val load_addr_f64 : string -> ptr Atom.t -> int -> float64 Atom.t * 'ret Instr.t
+val store_addr : 'a Atom.t -> ptr Atom.t -> int -> 'ret Instr.t
+val alloca : string -> int64 Atom.t -> ptr Atom.t * 'ret Instr.t
+val cast : string -> Type.t -> 'a Atom.t -> 'b Atom.t * 'ret Instr.t
+val call0 : string -> ('ret, 'ret) Fn.t -> 'ret Atom.t * 'block Instr.t
+val call1
+  :  string
+  -> ('a -> 'ret, 'ret) Fn.t
+  -> 'a Atom.t
+  -> 'ret Atom.t * 'block Instr.t
 
 val call2
   :  string
-  -> ('a -> 'b -> 'ret) Fn.t
+  -> ('a -> 'b -> 'ret, 'ret) Fn.t
   -> 'a Atom.t
   -> 'b Atom.t
-  -> 'ret Atom.t * Instr.t
+  -> 'ret Atom.t * 'block Instr.t
