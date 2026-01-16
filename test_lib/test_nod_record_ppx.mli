90126e9d7fc1951f00a604e9027e4ddd56414c77
Evan Williams <evanawilliams@live.com>
2026-01-16 21:30:44 +0800

    😊

--- test_lib/test_nod_record_ppx.mli
+++ test_lib/test_nod_record_ppx.mli
@@ -14,4 +14,24 @@ type point' =
   }
 [@@deriving_inline nod_record]
 
+include sig
+  [@@@ocaml.warning "-32"]
+
+  type nonrec point'_tuple_alias = point_tuple_alias * Dsl.int64
+
+  type nonrec point'_t =
+    { repr : (point_tuple_alias * Dsl.int64) Dsl.Type_repr.t
+    ; point :
+        ( point_tuple_alias * Dsl.int64
+          , point_tuple_alias
+          , point_t
+          , Dsl.record )
+          Dsl.Field.t
+    ; x : (point_tuple_alias * Dsl.int64, Dsl.int64, unit, Dsl.base) Dsl.Field.t
+    }
+
+  val point' : point'_t
+end
+[@@ocaml.doc "@inline"]
+
 [@@@end]
