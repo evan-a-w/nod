a012daa8543eea3e52734b662231c55a23a9f07c
Evan Williams <evanawilliams@live.com>
2026-01-25 01:07:30 +0800

    😊

---
+++ ir/program.ml
@@ -0,0 +1,14 @@
+open! Core
+open! Import
+
+type ('var, 'block) t =
+  { globals : Global.t list
+  ; functions : ('var, 'block) Function.t String.Map.t
+  }
+[@@deriving sexp]
+
+let map_functions t ~f = { t with functions = Map.map t.functions ~f }
+
+let map_function_roots t ~f =
+  { t with functions = Map.map t.functions ~f:(Function.map_root ~f) }
+;;
